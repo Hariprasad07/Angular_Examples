07d8ded2527294ecc44b0986edfedeb73b4c2d54
Hariprasad07 <harip7007@gmail.com>
2018-12-18 13:08:14 +0530

    first angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,47 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  devName:string="hari";
+  devName1:String="prasad";
+
+   constructor()
+   {
+      console.log("invoking constructor");
+   } 
+
+  //calculator example
+  num1:number=10;
+  num2:number=20;
+  result:number;
+
+  birds:object=[{"name":"dove","color":"white"},{"name":"dove","color":"white"}];
+
+  sum(n1:number,n2:number){
+    this.result=n1+n2;
+  }
+
+  add()
+  {
+    //this.result=this.num1+this.num2; 
+    this.sum(this.num1,this.num2);  //this line is calling for sum function
+  }
+
+  sub()
+  {
+    this.result=this.num1-this.num2;
+  }
+
+  mul()
+  {
+    this.result=this.num1*this.num2;
+  }
+
+  div()
+  {
+    this.result=this.num1/this.num2;
+
+  }
+
 }
